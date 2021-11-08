6424d55e92ec2ffb4ea511271d8396af69c04e93
José Victor Macedo Veloso <vitovls37@gmail.com>
2021-11-08 16:06:12 -0300

    Adicionando logica para renderizar input de pesquisa - Req 12 :heavy_check_mark:

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,32 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
+import SearchBar from './SearchBar';
 import './Styles/Header.css';
 
-const Header = ({ titlePage }) => (
-  <header>
-    <Link className="link-icon" to="/perfil">
-      <img src={ profileIcon } data-testid="profile-top-btn" alt="profile-icon" />
-    </Link>
-    <h1 data-testid="page-title">{titlePage}</h1>
-    <img src={ searchIcon } alt="search-icon" data-testid="search-top-btn" />
-  </header>
-);
+const Header = ({ titlePage }) => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <header>
+      <section className="icons-header">
+        <Link className="link-icon" to="/perfil">
+          <img src={ profileIcon } data-testid="profile-top-btn" alt="profile-icon" />
+        </Link>
+        <h1 data-testid="page-title">{titlePage}</h1>
+        <button type="button" onClick={ () => setVisible(!visible) }>
+          <img
+            src={ searchIcon }
+            alt="search-icon"
+            data-testid="search-top-btn"
+          />
+        </button>
+      </section>
+      {visible && <SearchBar />}
+    </header>
+  );
+};
 
 Header.propTypes = {
   titlePage: PropTypes.string.isRequired,
