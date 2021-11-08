8caee55bc2a89f84f89f7fda0231fe6ba9d5ff7f
Matheus Kaffka <matheus.kaffka@gmail.com>
2021-11-08 14:23:17 -0300

    Criadas as rotas do projeto

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
+    <div>
+    <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={}/>
+      <Route path="/comidas" component={} />
+      <Route path="/bebidas" component={} />
+      <Route path="/comidas/{id-da-receita}" component={}/>
+      <Route path="/bebidas/{id-da-receita}" component={}/>
+      <Route path="/comidas/{id-da-receita}/in-progress" component={}/>
+      <Route path="/bebidas/{id-da-receita}/in-progress" component={}/>
+      <Route path="/explorar" component={}/>
+      <Route path="/explorar/comidas" component={}/>
+      <Route path="/explorar/bebidas" component={}/>
+      <Route path="/explorar/comidas/ingredientes" component={}/>
+      <Route path="/explorar/bebidas/ingredientes" component={}/>
+      <Route path="/explorar/comidas/area" component={}/>
+      <Route path="/perfil" component={}/>
+      <Route path="/receitas-feitas" component={}/>
+      <Route path="/receitas-favoritas" component={}/>
+    </Switch>
+    </BrowserRouter>
     </div>
   );
 }
