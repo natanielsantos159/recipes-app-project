3cf17b32ef0a1d9721cef7ca0c68fdefd37fb552
Matheus Kaffka <matheus.kaffka@gmail.com>
2021-11-08 18:14:34 -0300

    Requisito 13 finalizado

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,49 @@
 import React from 'react';
 
 const SearchBar = () => (
-  <section>
+  <form>
     <input
       type="text"
       data-testid="search-input"
       placeholder="Buscar Receita!"
     />
-  </section>
+    <label htmlFor="ingredient">
+      Ingrediente:
+      <input
+        name="filter"
+        type="radio"
+        id="ingredient"
+        value="ingredient"
+        data-testid="ingredient-search-radio"
+      />
+    </label>
+    <label htmlFor="name">
+      Nome:
+      <input
+        name="filter"
+        type="radio"
+        id="name"
+        value="name"
+        data-testid="name-search-radio"
+      />
+    </label>
+    <label htmlFor="first-letter">
+      Primeira Letra:
+      <input
+        name="filter"
+        type="radio"
+        id="first-letter"
+        value="first-letter"
+        data-testid="first-letter-search-radio"
+      />
+    </label>
+    <button
+      type="button"
+      data-testid="exec-search-btn"
+    >
+      Buscar
+    </button>
+  </form>
 );
 
 export default SearchBar;
