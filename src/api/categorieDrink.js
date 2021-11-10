89d8da1336be3364f17baff1a6b3453eba2b9be6
Matheus Kaffka <matheus.kaffka@gmail.com>
2021-11-10 18:14:33 -0300

    Requisito 29 e 30 finalizados

--- src/api/categorieDrink.js
+++ src/api/categorieDrink.js
@@ -10,3 +10,9 @@ export const fetchFilterByCategorieDrink = async (categorie) => {
   const json = await response.json();
   return json.drinks;
 };
+
+export const fetchAllDrinks = async () => {
+  const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+  const json = await response.json();
+  return json.drinks;
+};
