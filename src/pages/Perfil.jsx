bc0e22f31e676000b9fabf5c27a9eb8b5e30c2eb
Nataniel Santos <nathan.santos159@hotmail.com>
2021-11-11 15:59:30 -0300

    Criando tela do perfil - Requisito 82, 84, 85 e 86

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import AppContext from '../context/AppContext';
 
-const Perfil = () => (
-  <main>
-    <Header titlePage="Perfil" show={ false } />
-    <Footer />
-  </main>
-);
+const Perfil = () => {
+  const { email } = useContext(AppContext);
+  return (
+    <main>
+      <Header titlePage="Perfil" show={ false } />
+      <p data-testid="profile-email">{email}</p>
+      <Link to="/receitas-feitas">
+        <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
+      </Link>
+      <Link to="/receitas-favoritas">
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Receitas Favoritas
+        </button>
+      </Link>
+      <Link to="/">
+        <button type="button" data-testid="profile-logout-btn">Sair</button>
+      </Link>
+      <Footer />
+    </main>
+  );
+};
 
 export default Perfil;
