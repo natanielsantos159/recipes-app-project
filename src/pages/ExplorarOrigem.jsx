1be98da5a7987d7cc2dccb537949ea292a8a3546
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-11-15 16:24:38 -0300

    Impl: Explorar Comidas por Area. Requisito 78

--- src/pages/ExplorarOrigem.jsx
+++ src/pages/ExplorarOrigem.jsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { fetchAllFoods, fetchMealsArea } from '../api/meals';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import AppContext from '../context/AppContext';
+import { renderCards } from './Comidas';
 
-const explorarOrigem = () => (
-  <main>
-    <Header titlePage="Explorar Origem" />
-    <Footer />
-  </main>
-);
+const useExplorarOrigem = () => {
+  const { foods, area, setArea, setFoods } = useContext(AppContext);
 
-export default explorarOrigem;
+  useEffect(() => {
+    fetchMealsArea().then(setArea);
+    fetchAllFoods().then(setFoods);
+  }, [setArea, setFoods]);
+
+  const max = 12;
+
+  const options = (currentArea) => (
+    <option data-testid={ `${currentArea.strArea}-option` } value={ currentArea.strArea }>
+      { currentArea.strArea }
+    </option>
+  );
+
+  const mainFoods = (foods.length > max ? foods.slice(0, max)
+    .map(renderCards) : foods.map(renderCards));
+
+  return (
+    <main>
+      <Header titlePage="Explorar Origem" />
+      <select data-testid="explore-by-area-dropdown">
+        <option data-testid="All-option" value="All">All</option>
+        {area.map(options)}
+      </select>
+      {mainFoods}
+      <Footer />
+    </main>
+  );
+};
+
+export default useExplorarOrigem;
